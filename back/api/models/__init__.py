b68172042966a2d3ba4447a8751a8b28b9ade10c
8harifi <8harifi@gmail.com>
2026-05-13 17:31:07 +0330

    refactor: consolidate models into a single package

---
+++ back/api/models/__init__.py
@@ -0,0 +1,11 @@
+from .lookups import *
+from .user import *
+from .patient import *
+from .doctor import *
+# from .organizations import *
+from .health_assistant import *
+# from .charity_center import *
+# from .service_center import *
+from .benefactor import *
+from .misc import *
+from .wallet import *
