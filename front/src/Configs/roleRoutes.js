931eb5ae37a8ba486474d7b627ec67f2c5abab05
mobin-84-rasa <mobinarasa84@gmail.com>
2026-06-02 12:48:55 +0330

    feat: add role-based route config

---
+++ front/src/Configs/roleRoutes.js
@@ -0,0 +1,10 @@
+export const roleRoutes = {
+  doctor: "/doctordashboard",
+  charity: "/charitabledashboard",
+  donor: "/dashboard",
+  patient: "/patientdashboard",
+  health_center: "/healthcenterdashboard",
+  salamatyar: "/salamatyardashboard",
+  admin: "/admin/dashboard",
+};
+///src\Configs\roleRoutes.js
